a6d8bc3b29cea7dcce18a1e9894b321988ec8ed3
GameGodS3 <sudevssuresh@gmail.com>
2022-04-22 13:46:55 +0530

    Added: Zoom Out on Scroll Down and Parallax

--- src/Theater.jsx
+++ src/Theater.jsx
@@ -1,27 +1,59 @@
-import React, { useState } from "react";
+import React, { createRef, useState } from "react";
 import "./styles/Theater.css";
+import logo from "../static/images/CET Film Society logo 1.svg";
+import downArrow from "../static/images/downArrow.svg";
 
-function Theater() {
-  const [debugFlag, setDebugFlag] = useState(false);
+function Theater(props) {
+  const [navbarDisplay, setNavbarDisplay] = useState(false);
+  const zoomFlag = true;
+
+  const scrollRef = createRef();
+  const zoomRef = createRef();
+  let zoom = 1;
+  const zoomSpeed = 0.01;
+  document.addEventListener("wheel", (e) => {
+    if (zoomFlag && zoom > 0.4) {
+      if (e.deltaY > 0) {
+        scrollRef.current.style.transform = `scale(${(zoom -= zoomSpeed)})`;
+      } else {
+        scrollRef.current.style.transform = `scale(${(zoom += zoomSpeed)})`;
+      }
+    } else {
+      zoomRef.current.style.overflowY = "scroll";
+    }
+  });
 
   // Add newer elements in a div with className parallax-group here.
   return (
-    <div className="Theater">
-      <button onClick={() => setDebugFlag(!debugFlag)}>Click Me</button>
-      <div
-        style={
-          debugFlag
-            ? { transform: "translate3d(700px, 0, -800px) rotateY(30deg)" }
-            : {}
-        }
-        className="parallax-group"
-      >
+    <div className="Theater" ref={zoomRef} style={{ overflowY: "hidden" }}>
+      <div className="parallax-group">
         <div className="parallax_layer screen"></div>
         <div className="parallax_layer seat-1"></div>
         <div className="parallax_layer seat-2"></div>
         <div className="parallax_layer seat-3"></div>
-        <div className="parallax_layer hero-text">
-          <h1>CET Film Society</h1>
+
+        <div
+          className="parallax_layer hero-text"
+          ref={scrollRef}
+          style={{ transition: "all 0.5s" }}
+        >
+          <div className="landing-cover">
+            <img src={logo} alt="CET Film Society Logo" />
+            <h1 className="club-title">CET Film Society</h1>
+            <h2 className="club-subtitle">
+              The official film club of College of Engineering Trivandrum
+            </h2>
+          </div>
+
+          <img
+            src={downArrow}
+            className="scroll-explore"
+            alt="Scroll to Explore"
+            onClick={() => {
+              props.navbarDisplay(navbarDisplay);
+              setNavbarDisplay(!navbarDisplay);
+            }}
+          />
         </div>
       </div>
       <div className="test parallax-group">Hello World</div>
