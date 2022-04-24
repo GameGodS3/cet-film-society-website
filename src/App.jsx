da8487a9fbfb6b7f861ecd68dce8a239102bbc83
GameGodS3 <sudevssuresh@gmail.com>
2022-04-24 23:01:16 +0530

    Added: Navbar appear on scroll only

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./styles/App.css";
 import Navbar from "./Navbar";
 import Theater from "./Theater";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [navbarDisplay, setNavbarDisplay] = useState(true);
+  const [navbarDisplay, setNavbarDisplay] = useState(false);
   const navToggle = (navDis) => {
     setNavbarDisplay(!navDis);
     return navbarDisplay;
   };
 
   return (
-    //      Inserting elements here in App.jsx after the Theater element might mess up. Refer Theater.jsx
     <div className="App">
       {navbarDisplay ? <Navbar /> : null}
-      <Theater navbarDisplay={navToggle} />
+      <Theater navbarDisplay={setNavbarDisplay} />
     </div>
   );
 }
