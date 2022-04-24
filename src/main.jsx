ab9fb42611e188fa3c95c859f97fbdf33600a20e
SouravSatheesh <souravsatheesh1@gmail.com>
2022-04-24 20:01:58 +0530

    :sparkles: About Page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Review from "./Review";
 import App from "./App";
 import "./styles/index.css";
+import About from "./About";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/reviews" element={<Review />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
