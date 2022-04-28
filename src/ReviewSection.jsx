1c815627fbea41d096d51733d4d48f3066700606
GameGodS3 <sudevssuresh@gmail.com>
2022-04-28 23:08:52 +0530

    Fixed: Unclosed tags

--- src/ReviewSection.jsx
+++ src/ReviewSection.jsx
@@ -8,35 +8,25 @@ import reviewList from "./reviewsFile.json";
 import "./styles/ReviewSection.css";
 
 function ReviewSection() {
-    return (
-        <div className="ReviewSection">
-            <div className="container py-5 row p-0 align-items-center justify-content-center mx-auto">
-                <div className="review-section-header">
-                    <div className="review-section-head-text">
-                        <h2 className="section-title">Popular Reviews of the week</h2>
-                        <div className="section-subtitle mb-3">
-                            Explore well curated film reviews from our community
-                        </div>
-                    </div>
-                    <div className="review-section-header-outlink">
-                        View All
-                        <Icon icon="ic:baseline-arrow-forward-ios" />
-                    </div>
-                </div>
-                <div className="review-cards my-4">
-                    <Link to={{ pathname: "/reviews", search: "?r01" }}>
-                        <ReviewCard />
-                    </Link>
-                </div>
+  return (
+    <div className="ReviewSection">
+      <div className="container py-5 row p-0 align-items-center justify-content-center mx-auto">
+        <div className="review-section-header">
+          <div className="review-section-head-text">
+            <h2 className="section-title">Popular Reviews of the week</h2>
+            <div className="section-subtitle mb-3">
+              Explore well curated film reviews from our community
             </div>
+          </div>
+          <div className="review-section-header-outlink">
+            <Link to={"/reviews"}>
+              View All
+              <Icon icon="ic:baseline-arrow-forward-ios" />
+            </Link>
+          </div>
         </div>
-        <div className="review-section-header-outlink">       
-        </div>
-          <Link to={'/reviews'}>
-            View All 
-            <Icon icon="ic:baseline-arrow-forward-ios" />
-          </Link>
-        </div>
+      </div>
+      <div className="review-section-header-outlink"></div>
       <div className="review-cards">
         {reviewList.reviews?.map((review, index) => {
           if (index <= 2) return <ReviewCard reviewId={review.reviewId} />;
