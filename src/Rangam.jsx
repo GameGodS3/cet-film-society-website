e1821e880880d024e05452e5e975ad9abb031aa1
GameGodS3 <sudevssuresh@gmail.com>
2022-04-30 14:36:06 +0530

    Added: Carousel

--- src/Rangam.jsx
+++ src/Rangam.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Icon } from "@iconify/react";
 
 import Navbar from "./Navbar";
@@ -7,44 +7,79 @@ import Footer from "./Footer";
 import "./styles/Rangam.css";
 
 function Rangam() {
-  let catImagesArray = [
-    "http://picsum.photos/200/300",
-    "http://picsum.photos/100/200",
-    "http://picsum.photos/400/600",
-    "http://picsum.photos/500/800",
-    "http://picsum.photos/60/100",
-    "http://picsum.photos/60/100",
-    "http://picsum.photos/60/100",
-    "http://picsum.photos/60/100",
+  let moviePosters = [
+    { name: "Sample Movie 1", poster: "http://via.placeholder.com/100.jpg" },
+    { name: "Sample Movie 2", poster: "http://via.placeholder.com/200.jpg" },
+    { name: "Sample Movie 3", poster: "http://via.placeholder.com/300.jpg" },
+    { name: "Sample Movie 4", poster: "http://via.placeholder.com/400.jpg" },
+    { name: "Sample Movie 5", poster: "http://via.placeholder.com/500.jpg" },
+    { name: "Sample Movie 6", poster: "http://via.placeholder.com/600.jpg" },
+    { name: "Sample Movie 7", poster: "http://via.placeholder.com/700.jpg" },
   ];
+  let index = 5;
+
+  useEffect(() => {
+    document.querySelector(".rangam-movie-name").innerHTML =
+      moviePosters[index - 2].name;
+  }, [index]);
+
+  const fetchNextImage = () => {
+    const image = document.createElement("img");
+    if (index >= moviePosters.length) {
+      index = 0;
+    }
+    image.src = moviePosters[index].poster;
+    image.className = "moviePoster";
+    index++;
+    return image;
+  };
+  const fetchPrevImage = () => {
+    const image = document.createElement("img");
+    if (index >= moviePosters.length) {
+      index = 0;
+    }
+    image.src = moviePosters[index].poster;
+    image.className = "moviePoster";
+    index++;
+    return image;
+  };
 
   const nextCarouselImage = () => {
     let posters = document.querySelector(".carousel-container");
     const lastPoster = posters.lastChild;
     posters.removeChild(lastPoster);
-    posters.prepend(lastPoster);
+    posters.prepend(fetchNextImage());
   };
   const prevCarouselImage = () => {
     let posters = document.querySelector(".carousel-container");
     const firstPoster = posters.firstChild;
     posters.removeChild(firstPoster);
-    posters.appendChild(firstPoster);
+    posters.appendChild(fetchPrevImage());
   };
 
   return (
     <div className="Rangam">
       <div className="rangam-carousel">
-        <Navbar /> <div className="section-title">Page Under Construction</div>
-        <button onClick={nextCarouselImage}>
+        <Navbar />{" "}
+        <div className="section-title rangam-movie-name">
+          Page Under Construction
+        </div>
+        <button onClick={prevCarouselImage}>
           {" "}
           <Icon icon="ic:baseline-arrow-back-ios" />
         </button>
         <div className="carousel-container">
-          {catImagesArray.slice(0, 5).map((cat) => {
-            return <img src={cat} alt="Movie Poster" className="moviePoster" />;
+          {moviePosters.slice(0, 5).map((movie) => {
+            return (
+              <img
+                src={movie.poster}
+                alt="Movie Poster"
+                className="moviePoster"
+              />
+            );
           })}
         </div>
-        <button onClick={prevCarouselImage}>
+        <button onClick={nextCarouselImage}>
           {" "}
           <Icon icon="ic:baseline-arrow-forward-ios" />
         </button>
